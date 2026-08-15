cfc9affa4ed16606ec9b31e5afabe648688e0509
alfeena-01 <alfeenahamza54321@gmail.com>
2026-08-15 21:08:24 +0530

    Update traffic_prediction notebook

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -201,6 +201,4 @@ heatmap_layer = pdk.Layer(
     "HeatmapLayer",
     data=traffic_filtered.dropna(subset=["latitude","longitude"]),
        get_position=["longitude","latitude"],
-            get_weight=weight_col,
-            radiusPixels=60,
-        )
+         
